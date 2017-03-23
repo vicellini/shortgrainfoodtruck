0392584c89af1caedf276e4dde574c28b416174d
Travis Hubbard <t3patterson@gmail.com>
2017-03-23 13:20:13 -0400

    updating to remove link to stylesheet in localhost

--- src/index.js
+++ src/index.js
@@ -1,2 +1,8 @@
+if(window.location.hostname === 'localhost'){
+	let linkEl = document.querySelector('link[href="./css/styles.css"]')
+	let headEl = document.querySelector('head')
+	headEl.removeChild(linkEl)
+}
+
 import styles from './styles/main.scss'
 import app from './scripts/app.js'
